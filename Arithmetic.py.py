c52ee7e3feece21713ece193b5dcc1a3571f0cde
SAI ARJUN <112474122+SAI-ARJUN@users.noreply.github.com>
2022-09-01 14:11:40 +0530

    Add files via upload

---
+++ Arithmetic.py.py
@@ -0,0 +1,16 @@
+a=int(input("Enter the value for a "))
+b=int(input("Enter the value for b"))
+c=a+b
+print(c)
+d=a-b
+print(d)
+e=a*b
+print(e)
+f=a/b
+print(f)
+g=a//b
+print(g)
+h=a%b
+print(h)
+i=a**b
+print(i)
